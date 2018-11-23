ba9358a4037aabcb0853314baba9e54c2c32845a
Diego <dlopezo@gmail.com>
2018-11-22 22:07:01 -0300

    22/11/2018

---
+++ gs-rest-service-complete/src/main/java/hello/Nacionalidades.java
@@ -0,0 +1,26 @@
+package hello;
+
+public class Nacionalidades {
+	
+	private int id;
+	private String nombre;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Nacionalidades(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+
+}
